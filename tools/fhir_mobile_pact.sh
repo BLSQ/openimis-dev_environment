a818fe4160d9008734dc44d16bb25623063abe18
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2023-06-08 11:28:32 +0200

    Makes the FHIR scenario works with HTTPS as the new release enforces it

--- tools/fhir_mobile_pact.sh
+++ tools/fhir_mobile_pact.sh
@@ -6,6 +6,8 @@ SESSION_DIR="${SCRIPT_DIR}/fhir_mobile_pact_session"
 
 [[ ! -d $SESSION_DIR ]] && mkdir "$SESSION_DIR"
 
+URL_ROOT="https://localhost"
+
 function urldecode() {
   : "${*//+/ }"
   echo -e "${_//%/\\x}"
@@ -21,7 +23,7 @@ function paginated_get_curl() {
   while
     filename="${SESSION_DIR}/$3_page${page}.json"
     echo -n " page ${page}"
-    curl -sX GET "${url}" \
+    curl -ksX GET "${url}" \
       -H "Authorization: Bearer ${token}" \
       -H "accept: application/json" \
       -H "Content-Type: application/json" \
@@ -44,7 +46,7 @@ echo -n "Check that the server is up (5 tries):"
 try=0
 success=0
 while
-  if curl -sI "http://localhost/api/" | grep 404 >/dev/null; then
+  if curl -ksI "${URL_ROOT}/api/" | grep 404 >/dev/null; then
     success=1
   else
     ((try++))
@@ -64,14 +66,14 @@ echo " UP!"
 
 # login and retrieve JWT
 echo "login"
-# curl -sX POST "http://localhost/api/graphql" \
+# curl -ksX POST "${URL_ROOT}/api/graphql" \
 #   -H "accept: application/json" \
 #   -H "Content-Type: application/json" \
 #   -c "${COOKIE_FILE}" \
 #   -d @"${SCRIPT_DIR}/fhir_mobile_pact_files/authenticate.json.graphql"
 
 token=$(
-  curl -sX POST "http://localhost/api/api_fhir_r4/login/" \
+  curl -ksX POST "${URL_ROOT}/api/api_fhir_r4/login/" \
     -H "accept: application/json" \
     -H "Content-Type: application/json" \
     -d @"${SCRIPT_DIR}/fhir_mobile_pact_files/authenticate.json" | jq -r '.token'
@@ -82,13 +84,13 @@ token=$(
 # https://openimis.atlassian.net/wiki/spaces/OP/pages/1389592716/FHIR+R4+-+Practitioner
 # similar to `claim/GetClaimAdmins` (not directly used in claim mobile app)
 paginated_get_curl "get practitioners (claim admins)" \
-  "http://localhost/api/api_fhir_r4/Practitioner/" \
+  "${URL_ROOT}/api/api_fhir_r4/Practitioner/" \
   "claim_admins" "${token}"
 
 # https://fhir.openimis.org/CodeSystem-diagnosis-ICD10-level1.html
 # similar to a part of `claim/GetDiagnosesServicesItems`
 echo "get code systems"
-curl -sX GET "http://localhost/api/api_fhir_r4/CodeSystem/diagnosis/" \
+curl -ksX GET "${URL_ROOT}/api/api_fhir_r4/CodeSystem/diagnosis/" \
   -H "accept: application/json" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${token}" \
@@ -99,7 +101,7 @@ curl -sX GET "http://localhost/api/api_fhir_r4/CodeSystem/diagnosis/" \
 # `claim/GetPaymentLists`
 
 paginated_get_curl "get services (diagnoses services items and payment list)" \
-  "http://localhost/api/api_fhir_r4/ActivityDefinition/" \
+  "${URL_ROOT}/api/api_fhir_r4/ActivityDefinition/" \
   "claim_services" "${token}"
 
 # https://openimis.atlassian.net/wiki/spaces/OP/pages/1400045588/FHIR+R4+-+Medication
@@ -107,34 +109,34 @@ paginated_get_curl "get services (diagnoses services items and payment list)" \
 # `claim/GetPaymentLists`
 
 paginated_get_curl "get medications (diagnoses services items and payment list):" \
-  "http://localhost/api/api_fhir_r4/Medication/" \
+  "${URL_ROOT}/api/api_fhir_r4/Medication/" \
   "claim_medications" "${token}"
 
 # https://openimis.atlassian.net/wiki/spaces/OP/pages/1389592652/FHIR+R4+-+ClaimResponse
-# similar to `http://localhost/api/claim/GetClaims` list or get on a given id
+# similar to `${URL_ROOT}/api/claim/GetClaims` list or get on a given id
 
 paginated_get_curl "get claims:" \
-  "http://localhost/api/api_fhir_r4/ClaimResponse/" \
+  "${URL_ROOT}/api/api_fhir_r4/ClaimResponse/" \
   "claim_responses" "${token}"
 
 # https://www.hl7.org/fhir/organization.html
 # to retrieve health facilities (not directly used in claim mobile app)
 
 paginated_get_curl "get organizations (health facilities):" \
-  "http://localhost/api/api_fhir_r4/Organization/" \
+  "${URL_ROOT}/api/api_fhir_r4/Organization/" \
   "claim_organizations" "${token}"
 
 # https://openimis.atlassian.net/wiki/spaces/OP/pages/1389592724/FHIR+R4+-+PractitionerRole
 # Neeeded to link practitioner to their organization
 paginated_get_curl "get practitioner roles" \
-  "http://localhost/api/api_fhir_r4/PractitionerRole/" \
+  "${URL_ROOT}/api/api_fhir_r4/PractitionerRole/" \
   "claim_pracitioner_roles" "${token}"
 
 # https://openimis.atlassian.net/wiki/spaces/OP/pages/1389133931/FHIR+R4+-+Patient
 # similar to `insuree/{chfid}` but list or direct ID
 
 echo "get patients (insuree)"
-curl -sX GET "http://localhost/api/api_fhir_r4/Patient/" \
+curl -ksX GET "${URL_ROOT}/api/api_fhir_r4/Patient/" \
   -H "Authorization: Bearer ${token}" \
   -H "accept: application/json" \
   -H "Content-Type: application/json" \
@@ -144,7 +146,7 @@ curl -sX GET "http://localhost/api/api_fhir_r4/Patient/" \
 # similar to `insuree/{chfid}/enquire` but list or direct ID
 
 echo "get coverages (enquire)"
-curl -sX GET "http://localhost/api/api_fhir_r4/Coverage/" \
+curl -ksX GET "${URL_ROOT}/api/api_fhir_r4/Coverage/" \
   -H "Authorization: Bearer ${token}" \
   -H "accept: application/json" \
   -H "Content-Type: application/json" \
